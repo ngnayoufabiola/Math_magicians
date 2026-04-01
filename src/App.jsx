06cb1bc3703492200697d54484e115518a46e301
ngnayoufabiola <ngnayoufabiola@gmail.com>
2026-04-01 13:32:43 +0100

    Fix import path casing for Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './Components/Header';
 import Home from './Components/Home';
 import CalculatorPage from './Components/CalculatorPage';
 import Quote from './Components/Quote';
